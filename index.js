6c607fba576ca4d624e785996648d7bda54cdd22
Swaraj-kumar <97353264+Swaraj-kumar@users.noreply.github.com>
2023-07-20 11:46:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css"
-import Card from "./Cards";
-import Sdata from "./Sdata";
+import "./index.css";
+import App from './App';
 
-function NetflixCard(val){
-    console.log(val);
-    return(
-    <Card
-        imgsrc = {val.imgscr}
-        title = {val.title}
-        sname = {val.sname}
-        link = {val.links}
-    />
-    );
-}
-
-ReactDOM.render(
-    <>
-    <h1 className="heading_style">List of Top 5 Netflix Series</h1>
-    {Sdata.map(NetflixCard)}
-    </>
-,document.getElementById("root")
-);
+ReactDOM.render(<App/>,document.getElementById("root"));
 
 
 
