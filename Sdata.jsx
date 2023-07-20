ebbe9eeeb5534eeb79359cb32ab59f121e666251
Swaraj-kumar <97353264+Swaraj-kumar@users.noreply.github.com>
2023-07-20 11:42:46 +0530

    Sdata.jsx

--- Sdata.jsx
+++ Sdata.jsx
@@ -1,29 +1,34 @@
 const Sdata = [
     {
+        id : 0,
         sname : "DARK",
         imgscr : "https://e0.pxfuel.com/wallpapers/343/141/desktop-wallpaper-dark-the-official-guide-dark-web-series.jpg",
         title : "A Netflix original Series",
         links : "https://www.netflix.com/in/title/80100172",
     },
     {
+        id : 1,
         sname : "THE VAMPIRE DIARIES",
         imgscr : "https://assets.change.org/photos/3/qg/fc/JwqGFCrysZPzBqp-800x450-noPad.jpg?1550716639",
         title : "A Netflix original Series",
         links : "https://www.netflix.com/in/title/70143860",
     },
     {
+        id : 2,
         sname : "SQUID GAME",
         imgscr : "https://phantom-marca.unidadeditorial.es/116e84ce65bfff4870e430574c03186b/crop/0x0/1597x899/resize/1200/f/webp/assets/multimedia/imagenes/2021/10/01/16330974723192.png",
         title : "A Netflix original Series",
         links : "https://www.netflix.com/in/title/81040344",
     },
     {
+        id : 3,
         sname : "STRANGER THINGS",
         imgscr : "https://images.ctfassets.net/4cd45et68cgf/22eaxyrfqLTOmD0ZgFJDX0/6d7b8a0f4c3130fd87c9921cbd11d180/image5.jpg",
         title : "A Netflix original Series",
         links : "https://www.netflix.com/in/title/80057281",
     },
     {
+        id : 4,
         sname : "WEDNESDAY",
         imgscr : "https://www.bollywoodhungama.com/wp-content/uploads/2022/12/Jenna-Ortegas-Wednesday-becomes-Netflixs-third-most-popular-English-language-series-of-all-time-with-752.52-million-hours-viewed.jpg",
         title : "A Netflix original Series",
@@ -31,4 +36,3 @@ const Sdata = [
     }
 ]
 export default Sdata;
-
